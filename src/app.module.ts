f2f9a49f267600a391d64bbdfef1eaa4d0c29576
yasaichi <yasaichi@users.noreply.github.com>
2024-02-26 21:06:16 +0900

    Introduce `Effect`

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 import { AppController } from './app.controller.ts';
+import { AppInterceptor } from './app.interceptor.ts';
 import { AppService } from './app.service.ts';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, {
+    provide: APP_INTERCEPTOR,
+    useClass: AppInterceptor,
+  }],
 })
 export class AppModule {}
