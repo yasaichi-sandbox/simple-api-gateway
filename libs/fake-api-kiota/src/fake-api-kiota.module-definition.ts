7e6743f041edaa486e166b51572abc78c69fedca
yasaichi <yasaichi@users.noreply.github.com>
2024-03-10 15:43:08 +0900

    "Effective" implementation using `Effect`

--- libs/fake-api-kiota/src/fake-api-kiota.module-definition.ts
+++ libs/fake-api-kiota/src/fake-api-kiota.module-definition.ts
@@ -2,4 +2,8 @@ import { ConfigurableModuleBuilder } from '@nestjs/common';
 import { type FakeApiKiotaModuleOptions } from './interfaces/fake-api-kiota-module-options.interface.ts';
 
 export const { ConfigurableModuleClass, OPTIONS_TYPE } =
-  new ConfigurableModuleBuilder<FakeApiKiotaModuleOptions>().build();
+  new ConfigurableModuleBuilder<FakeApiKiotaModuleOptions>()
+    .setExtras(
+      { global: true },
+      (definition, { global }) => ({ ...definition, global }),
+    ).build();
